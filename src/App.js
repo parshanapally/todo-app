e1758713cb6e6c35f37be112ed90a42c7d9cd6fd
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-01-27 10:08:33 -0600

    style: add css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
 
 class App extends Component {
+  state = {
+    noteText: ""
+  };
+
+  updateNoteText = e => {
+    this.setState({ noteText: e.target.value });
+  };
+
+  handleKeyPress = () => {};
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="header">React ToDo Application</div>
+        <div className="btn">+</div>
+        <input
+          type="text"
+          className="text-input"
+          ref={input => {
+            this.textInput = input;
+          }}
+          value={this.state.noteText}
+          onChange={noteText => this.updateNoteText(noteText)}
+          onKeyPress={this.handleKeyPress.bind(this)}
+        />
       </div>
     );
   }
