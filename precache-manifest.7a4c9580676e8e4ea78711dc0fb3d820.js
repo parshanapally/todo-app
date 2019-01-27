2b5dbb9bdb2d98552620978bae95f75128d2df04
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-01-27 10:52:24 -0600

    Updates

---
+++ precache-manifest.7a4c9580676e8e4ea78711dc0fb3d820.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "c944167141abae92e8c8",
+    "url": "/todo-app/static/js/main.c9441671.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/todo-app/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "c944167141abae92e8c8",
+    "url": "/todo-app/static/css/main.4de8f05f.chunk.css"
+  },
+  {
+    "revision": "27e63a5e78adfa013520b9f15435de7e",
+    "url": "/todo-app/index.html"
+  }
+];
